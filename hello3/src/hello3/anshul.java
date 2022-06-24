a463bd42496cbfa72c379916775893594a54dc69
0302anshulrana <91405443+0302anshulrana@users.noreply.github.com>
2022-06-24 11:33:44 +0530

    Update anshul.java

--- hello3/src/hello3/anshul.java
+++ hello3/src/hello3/anshul.java
@@ -3,7 +3,7 @@ package hello3;
 public class anshul {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		System.out.println("hello world this is anshul rana");
 	}
 
 }
